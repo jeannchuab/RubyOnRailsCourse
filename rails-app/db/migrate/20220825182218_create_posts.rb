ed1ccf7acbdb6261e472ffceb20016c8cf9a396b
Jeann Luiz Chuab <jeann.chuab@gmail.com>
2022-08-25 15:30:58 -0300

    Initial git setup

---
+++ rails-app/db/migrate/20220825182218_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :summary
+      t.text :body
+      t.boolean :active
+
+      t.timestamps
+    end
+  end
+end
