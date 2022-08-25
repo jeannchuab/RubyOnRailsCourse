551351b21ceb147843660686750b5912fc525b6e
Jeann Luiz Chuab <jeann.chuab@gmail.com>
2022-08-25 18:42:44 -0300

    Deleting cache

--- rails-app/db/migrate/20220825182218_create_posts.rb
+++ rails-app/db/migrate/20220825182218_create_posts.rb
@@ -4,7 +4,7 @@ class CreatePosts < ActiveRecord::Migration[7.0]
       t.string :title
       t.string :summary
       t.text :body
-      t.boolean :active
+      t.boolean :active, default: true
 
       t.timestamps
     end
