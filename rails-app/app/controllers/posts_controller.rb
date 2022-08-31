0b3c4f6c49f12627a4be5dd60493f882710f78c9
Jeann Luiz Chuab <jeann.chuab@gmail.com>
2022-08-31 16:47:46 -0300

    Adding new routes and menu.

--- rails-app/app/controllers/posts_controller.rb
+++ rails-app/app/controllers/posts_controller.rb
@@ -17,6 +17,7 @@ class PostsController < ApplicationController
 
   # GET /posts/1/edit
   def edit
+    session[:return_to] ||= request.referer
   end
 
   # POST /posts or /posts.json
@@ -24,12 +25,10 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
 
     respond_to do |format|
-      if @post.save
-        format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
-        format.json { render :show, status: :created, location: @post }
+      if @post.save        
+        redirect_to post_url(@post), notice: "Post was successfully created."
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
+        render :new
       end
     end
   end
@@ -38,11 +37,9 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to post_url(@post), notice: "Post was successfully updated." }
-        format.json { render :show, status: :ok, location: @post }
+        redirect_to post_url(@post), notice: "Post was successfully updated."
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
+        render :edit
       end
     end
   end
@@ -52,8 +49,7 @@ class PostsController < ApplicationController
     @post.destroy
 
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
-      format.json { head :no_content }
+      redirect_to posts_url, notice: "Post was successfully destroyed."
     end
   end
 
--- rails-app/config/routes.rb
+++ rails-app/config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'welcome/home'
   get 'welcome/features'
-  get 'welcome/about'
-  get 'welcome/contact'
+  get 'welcome/about', to: 'public#about', as: 'about' #So we can use as about_path
+  get 'welcome/contact', to: "public#contact", as: 'contact' #So we can use as contact_path
+  get 'welcome/blog', to: "public#blog", as: 'blog' #So we can use as blog_path
+
   resources :posts
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "public#homepage"
